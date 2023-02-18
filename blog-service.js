4c1a172d5d0c3521c004552bc2ae833d8767173b
Sajidur Rahman <msrahman33@myseneca.ca>
2023-02-18 17:46:06 -0500

    a3

--- blog-service.js
+++ blog-service.js
@@ -42,3 +42,55 @@ module.exports.getCategories = function () {
     else resolve(categories);
   });
 };
+
+module.exports.getPostsByCategory = (category) => {
+  return new Promise((resolve, reject) => {
+    var PostsByCategory = posts.filter((post) => post.category == category);
+    PostsByCategory.length == 0
+      ? reject("No posts found")
+      : resolve(PostsByCategory);
+  });
+};
+
+module.exports.getPostsByMinDate = (minDateStr) => {
+  return new Promise((resolve, reject) => {
+    const minDate = new Date(minDateStr);
+
+    const postsByMinDate = posts.filter(
+      (post) => new Date(post.postDate) >= minDate
+    );
+
+    postsByMinDate.length == 0
+      ? reject("No posts found")
+      : resolve(postsByMinDate);
+  });
+};
+
+module.exports.getPostById = (id) => {
+  return new Promise((resolve, reject) => {
+    const getId = posts.filter((post) => post.id == id);
+    getId.length == 0 ? reject("No posts found") : resolve(getId);
+  });
+};
+
+module.exports.adPost = function (postData) {
+  return new Promise((resolve, reject) => {
+    if (postData) {
+      postData.published = postData.published == undefined ? false : true;
+      postData.id = posts.length + 1;
+      posts.push(postData);
+      // posts.push({
+      //   id: posts.length,
+      //   body: postData.body,
+      //   title: postData.title,
+      //   postDate: postData.postDate,
+      //   category: postData.category,
+      //   featureImage: postData.featureImage,
+      //   published: postData.published,
+      // });
+      resolve(postData);
+    } else {
+      reject("Invalid");
+    }
+  });
+};
--- server.js
+++ server.js
@@ -3,7 +3,7 @@
  *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source
  *  (including 3rd party web sites) or distributed to other students.
  *
- *  Name: _________MD SAJIDUR RAHMAN_____________ Student ID: _____115695207_________ Date: _____1/31/2023___________
+ *  Name: _________MD SAJIDUR RAHMAN_____________ Student ID: _____115695207_________ Date: _____2/18/2023___________
  *
  *  Online (Cyclic) Link:
  *  https://outstanding-pear-giraffe.cyclic.app
@@ -15,6 +15,18 @@ var express = require("express");
 var app = express();
 
 var path = require("path");
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
+
+cloudinary.config({
+  cloud_name: "dboqoihuy",
+  api_key: 961783648495689,
+  api_secret: "_o1fNNfyTMaiuk_eGS91gv0qgzU",
+  secure: true,
+});
+
+const upload = multer();
 
 var HTTP_PORT = process.env.PORT || 8080;
 
@@ -47,14 +59,35 @@ app.get("/blog", function (req, res) {
 
 app.use(express.static("public"));
 app.get("/posts", function (req, res) {
+  const category = req.query.category;
+  const minDate = req.query.minDate;
+  if (category) {
+    blog
+      .getPostsByCategory(category)
+      .then((data) => res.send(data))
+      .catch((err) => res.send(err));
+  } else if (minDate) {
+    blog
+      .getPostsByMinDate(minDate)
+      .then((data) => res.send(data))
+      .catch((err) => res.send(err));
+  } else
+    blog
+      .getAllPosts()
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        res.send({ message: err });
+      });
+});
+
+app.use(express.static("public"));
+app.get("/post/:id", (req, res) => {
   blog
-    .getAllPosts()
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      res.send({ message: err });
-    });
+    .getPostById(req.params.id)
+    .then((data) => res.send(data))
+    .catch((err) => res.send(err));
 });
 
 app.use(express.static("public"));
@@ -69,8 +102,9 @@ app.get("/categories", function (req, res) {
     });
 });
 
-app.all("*", function (req, res) {
-  res.status(404).sendFile(path.join(__dirname, "/data/Error.jpg"));
+app.use(express.static("public"));
+app.get("/posts/add", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/addPost.html"));
 });
 
 blog
@@ -81,3 +115,47 @@ blog
   .catch((err) => {
     console.log(err);
   });
+app.use(express.static("public"));
+app.post("/posts/add", upload.single("featureImage"), (req, res) => {
+  let streamUpload = (req) => {
+    return new Promise((resolve, reject) => {
+      let stream = cloudinary.uploader.upload_stream((error, result) => {
+        if (result) {
+          resolve(result);
+        } else {
+          reject(error);
+        }
+      });
+
+      streamifier.createReadStream(req.file.buffer).pipe(stream);
+    });
+  };
+
+  async function upload(req) {
+    let result = await streamUpload(req);
+    console.log(result);
+    return result;
+  }
+
+  upload(req)
+    .then((uploaded) => {
+      req.body.featureImage = uploaded.url;
+
+      blog
+        .adPost(req.body)
+        .then((data) => {
+          //console.log(data);
+          res.redirect("/posts");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.all("*", function (req, res) {
+  res.status(404).sendFile(path.join(__dirname, "/data/Error.jpg"));
+});
